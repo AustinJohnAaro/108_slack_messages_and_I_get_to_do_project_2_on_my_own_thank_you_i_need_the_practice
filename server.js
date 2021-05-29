c44715fd3d2401a4441b3bcee899e1fae5b08bbe
AustinJohnAaro <austin1472a@gmail.com>
2021-05-28 22:28:53 -0700

    F#@$ you you  pice of S^&

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controller');
-
+const calendar = document.querySelector(".time-block")
 require('dotenv').config()
 
 
@@ -52,3 +52,60 @@ sequelize.sync({force: false}).then(() => {
   }) 
 })
 
+
+
+
+
+
+
+
+
+
+
+
+const isWeekend = day => {
+    return day % 7 === 6 || day % 7 ===0;
+
+}
+
+const getDayName = day => {
+    const date = new Date(Date.UTC(2021, 0, day));
+
+       return new Intl.DateTimeFormat("en-US", { weekday: "short" })
+    .format(date);
+}
+
+
+//console.log 
+for (let day = 1; day <= 31; day ++) {
+
+    let weekend = isWeekend(day)
+
+   let name = "";
+    if (day < 7){ 
+        const dayName = getDayName(day);
+        name = `<div class="name">${dayName}</div>`
+    }
+    
+}
+
+
+document.querySelectorAll("#time-block .day").forEach(day => { day.addEventListener("click", event => { event.currentTarget.classList.toggle("selected");});
+const isWeekend = day => {
+    return day % 7 === 6 || day % 7 ===0;
+
+}
+
+const getDayName = day => {
+    const date = new Date(Date.UTC(2021, 0, day));
+
+       return new Intl.DateTimeFormat("en-US", { weekday: "short" })
+    .format(date);
+}
+
+
+
+
+
+
+});
