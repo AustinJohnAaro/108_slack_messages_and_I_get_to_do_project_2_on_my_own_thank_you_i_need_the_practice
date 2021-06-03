e04628d0217b4558f6681ece86f5db28446b2518
AustinJohnAaro <austin1472a@gmail.com>
2021-06-03 12:39:27 -0700

    u

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+let addToDoAppButton = document.getElementById('addToDApp');
+let toDoContainer = document.getElementById('toDoContainer'); 
+let inputField = document.getElementById('inputField');
+
+addToDoAppButton.addEventListener('click', function(){
+  var paragraph = document.createElement('p')
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = inputField.Value;
+  toDoContainer.appendChild(paragraph);
+  inputField.value = "";
+  paragraph.addEventListener('click', function(){
+    paragraph.style.textDecoration = "line-through";
+  }) 
+  paragraph.addEventListener('dblclick', function(){
+    toDoContainer.removeChild(paragraph);
+  })
+})
+
+
+
+
 const path = require('path');
 const express = require('express'); 
 const session = require('express-session');
